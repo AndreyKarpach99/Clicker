96990686e36964f42110c96bb645ac25d8a695ab
Andrey Karpach <58387475+AndreyKarpach99@users.noreply.github.com>
2020-08-08 23:16:22 +0300

    Add files via upload

---
+++ clicker/index.js
@@ -0,0 +1,34 @@
+let cliker = 0;
+
+const TIMEOUT = 5000,
+  display = document.querySelector('#display'),
+  button = document.querySelector('#button'),
+  counter = document.querySelector('#counter');
+
+button.onclick = start;
+
+function start() {
+
+  const startTime = Date.now()
+
+  display.textContent = formatTime(TIMEOUT);
+
+  button.onclick = () => counter.textContent = cliker++;
+
+  const interval = setInterval(() => {
+    const delta = Date.now() - startTime;
+    display.textContent = formatTime(TIMEOUT - delta);
+  }, 100);
+
+  const timeout = setTimeout(() => {
+    button.onclick = null;
+    display.textContent = "Game Over";
+    clearInterval(interval);
+    clearTimeout(timeout);
+  }, TIMEOUT)
+
+  function formatTime(ms) {
+    return Number.parseFloat(ms / 1000).toFixed(2);
+  }
+
+}
